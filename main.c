3a500baeca789ce99b91515d88b57bc3b9069efb
Stanley Amaechi <stanley.amaechi@student.oulu.fi>
2024-06-10 23:03:36 +0300

    Add files via upload
    Getting started - Setting up development environment and debugging  this program...

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// the main function
+int main(){
+    int a;
+
+    printf("Hello there!\n");
+
+    a = 12;
+    printf("a=%d\n", a);
+
+    a = 13;
+    printf("a=%d\n", a);
+
+    return 0;
+}
