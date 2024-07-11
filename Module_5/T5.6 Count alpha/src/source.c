4ad2a4f57d4eb84e24943fe09761a116d419c68c
Stanley Amaechi <stanley.amaechi@student.oulu.fi>
2024-07-11 20:44:52 +0300

    Add files via upload
    T5.6 completed...

---
+++ Module_5/T5.6 Count alpha/src/source.c
@@ -0,0 +1,21 @@
+#include "source.h"
+#include <ctype.h>
+
+
+/**
+ * \brief Counts alphabetical characters in a string
+ *
+ * \param str The string to be processed
+ * \return The number of alphabetical characters
+ */
+int count_isalpha(const char *str)
+{
+     int count = 0;
+    while (*str != '\0') {
+        if (isalpha((unsigned char)*str)) {
+            count++;
+        }
+        str++;
+    }
+    return count;   
+}
